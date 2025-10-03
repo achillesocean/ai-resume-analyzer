b65f9d4835e5ee5078b0b85f96f7d82036f7d5d3
Felmeta Dereje <0202sent@gmail.com>
2025-10-03 17:29:31 +0300

    homepage resume showcase

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
+import { resumes } from "~/constants";
 import type { Route } from "./+types/home";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +13,19 @@ export default function Home() {
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track Your Applications & Resume Rating</h1>
         </div>
+        {resumes.length > 0 && (
+          <div
+            className="resumes-section
+        "
+          >
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
