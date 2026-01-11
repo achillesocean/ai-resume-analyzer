7a7aa382e1144ad2d3a89e183bd52ccabf0da6b4
Felmeta Dereje <0202sent@gmail.com>
2026-01-11 13:10:23 +0300

    fetch real resumes on the homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 type Props = { resume: Resume };
 
 function ResumeCard({
   resume: { id, companyName, jobTitle, feedback, imagePath },
 }: Props) {
-  // explain the destructuring of a destructuring!?
-  // then there is renaming after destructuring
-  // i think it doesn't recognize 'resume' anymore
+  const [resumeUrl, setResumeUrl] = useState("");
+  const { fs } = usePuterStore();
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [imagePath]);
+
   return (
     <Link
       to={`/resume/${id}`}
@@ -20,25 +32,34 @@ function ResumeCard({
       "
       >
         <div className="flex flex-col gap-2">
-          <h2 className=" !text-black font-bold break-words">
-            {/* explain the ! important property!? */}
-            {companyName}
-          </h2>
-          <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+          {companyName && (
+            <h2 className=" !text-black font-bold break-words">
+              {/* explain the ! important property!? */}
+              {companyName}
+            </h2>
+          )}
+          {jobTitle && (
+            <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+          )}
+          {!companyName && !jobTitle && (
+            <h2 className="!text-black font-bold">Resume</h2>
+          )}
         </div>
         <div className="flex-shrink-0">
           <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
-      <div className="gradient-border animate-in fade-in duration-1000">
-        <div className="w-full h-full">
-          <img
-            src={imagePath}
-            alt="resume"
-            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
-          />
+      {resumeUrl && (
+        <div className="gradient-border animate-in fade-in duration-1000">
+          <div className="w-full h-full">
+            <img
+              src={resumeUrl}
+              alt="resume"
+              className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </Link>
   );
 }
